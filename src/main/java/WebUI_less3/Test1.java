34710ede45ce5252b63c031cf52ae05a1d477996
UstinKonst <ustinovskyk@mail.ru>
2023-01-25 15:42:50 +0300

    Web UI start (1)

---
+++ src/main/java/WebUI_less3/Test1.java
@@ -0,0 +1,31 @@
+package WebUI_less3;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
+
+public class Test1 {
+
+//    Поиск фильма по фанру
+    public static void main( String[] args ) {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--incognito");
+        options.addArguments("start-maximized");
+
+
+//        WebDriver driver = new ChromeDriver(options);
+//        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+//        driver.get("https://www.ozon.ru/");
+
+//        try {
+//            Thread.sleep(3000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+
+    }
+}
